c6449fd685398d392d9dca0e16da52d0ccc37678
GITHUB Pac-Magician <83561919+Pac-MagicianGITHUB@users.noreply.github.com>
2022-01-19 09:22:54 -0500

    e

--- pacmanMINIWORLD8bit.c
+++ pacmanMINIWORLD8bit.c
@@ -1,3 +1,26 @@
-//Welcome to Pac-Man Mini World! [First Build Demo]
-//In this game you are playing as Pac-Man as he rescues his family from the evil scammer, as he go through 8 different worlds...
-//This game takes place in 202X
+///Welcome to Pac-Man Mini World! [First Build Demo] >')
+//In this game you are playing as Pac-Man as he rescues his family from an evil bootleger (who is a ghost), as he go through 8 different worlds... >')
+//This game takes place in 202X >')
+
+#include <stdlib.h>
+#include <stdlib.h>
+#include <cv.h>
+#include <cvu.h>
+
+#include "common.h"
+//#link "common.c"
+
+#include "PSGlib.h"
+//#link "PSGlib.c"
+
+//#link "bootup.c"
+//#resource "disclaimer.h"
+//#resource "copyright.h"
+//#resource "copyright2.h"
+//#resource "title.h"
+//#resource "1_1.h"
+//#resource "1_2.h"
+//#resource "Kinky_boss.h"
+//#resource "level_complete.h"
+//#resource "demo_end.h"
+
